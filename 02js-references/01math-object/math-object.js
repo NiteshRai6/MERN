b003a4631b698d824cb6cf33779fe3b4885f3de0
Nitesh <ndelhi66@gmail.com>
2024-07-17 18:18:59 +0530

    Math Object

---
+++ 02js-references/01math-object/math-object.js
@@ -0,0 +1,732 @@
+'use strict';
+
+// 1. Math sin() :
+// let result;
+
+// result = Math.sin(1);
+// console.log(result);
+
+// result = Math.sin(10);
+// console.log(result);
+
+// result = Math.sin(90);
+// console.log(result);
+
+// result = Math.sin('5');
+// console.log(result);
+
+// result = Math.sin('apple');
+// console.log(result);
+
+// result = Math.sin(Infinity);
+// console.log(result);
+
+// 2. Math cos() :
+// let result;
+// result = Math.cos(25);
+// console.log(result);
+// result = Math.cos(Math.PI);
+// console.log(result);
+
+// 3. Math tan() :
+// let result;
+// result = Math.tan(25);
+// console.log(result);
+// result = Math.tan(Math.PI);
+// console.log(result);
+
+// 4. Math.abs() :
+// let result;
+
+// result = Math.abs(8);
+// console.log(result);
+
+// result = Math.abs(-8);
+// console.log(result);
+
+// result = Math.abs('-8');
+// console.log(result);
+
+// result = Math.abs(8.876);
+// console.log(result);
+
+// 5. Math.pow() :
+// let result;
+
+// result = Math.pow(5, 3);
+// console.log(result);
+
+// result = Math.pow(1, 3);
+// console.log(result);
+
+// result = Math.pow(-2, 3);
+// console.log(result);
+
+// result = Math.pow(2, -3);
+// console.log(result);
+
+// result = Math.pow(-2, -3);
+// console.log(result);
+
+// result = Math.pow(5, 0);
+// console.log(result);
+
+// result = Math.pow(0, 5);
+// console.log(result);
+
+// result = Math.pow(0, -5);
+// console.log(result);
+
+// result = Math.pow(1, -5);
+// console.log(result);
+
+// result = Math.pow(2.765, 4);
+// console.log(result);
+
+// result = Math.pow(2.765, 4.1456);
+// console.log(result);
+
+// result = Math.pow('6', '5');
+// console.log(result);
+
+// result = Math.pow('a', 'b');
+// console.log(result);
+
+// 6. Math asin() :
+// let result;
+
+// result = Math.asin(0);
+// console.log(result);
+
+// result = Math.asin(1);
+// console.log(result);
+
+// result = Math.asin(-1);
+// console.log(result);
+
+// result = Math.asin(0.5);
+// console.log(result);
+
+// result = Math.asin(25);
+// console.log(result);
+
+// 7. Math acos() :
+// let result;
+
+// result = Math.acos(1);
+// console.log(result);
+
+// result = Math.acos(0);
+// console.log(result);
+
+// result = Math.acos(-1);
+// console.log(result);
+
+// result = Math.acos(0.5);
+// console.log(result);
+
+// result = Math.acos(25);
+// console.log(result);
+
+// 8. Math atan() :
+// let result;
+
+// result = Math.atan(1);
+// console.log(result);
+
+// result = Math.atan(0);
+// console.log(result);
+
+// result = Math.atan(-1);
+// console.log(result);
+
+// result = Math.atan(0.5);
+// console.log(result);
+
+// result = Math.atan(25);
+// console.log(result);
+
+// 9. Math ceil() :
+// let result;
+
+// result = Math.ceil(5.2);
+// console.log(result);
+
+// result = Math.ceil(5.9);
+// console.log(result);
+
+// result = Math.ceil(-5.2);
+// console.log(result);
+
+// result = Math.ceil('5.2');
+// console.log(result);
+
+// result = Math.ceil('apple');
+// console.log(result);
+
+// result = Math.ceil(null);
+// console.log(result);
+
+// 10. Math floor() :
+// let result;
+
+// result = Math.floor(5.2);
+// console.log(result);
+
+// result = Math.floor(5.9);
+// console.log(result);
+
+// result = Math.floor(-5.2);
+// console.log(result);
+
+// result = Math.floor('5.2');
+// console.log(result);
+
+// result = Math.floor('apple');
+// console.log(result);
+
+// result = Math.floor(null);
+// console.log(result);
+
+// 11. Math round() :
+// let result;
+
+// result = Math.round(5.2);
+// console.log(result);
+
+// result = Math.round(5.9);
+// console.log(result);
+
+// result = Math.round(-5.2);
+// console.log(result);
+
+// result = Math.round(-5.6);
+// console.log(result);
+
+// result = Math.round('5.2');
+// console.log(result);
+
+// result = Math.round('apple');
+// console.log(result);
+
+// result = Math.round(null);
+// console.log(result);
+
+// result = Math.round(5.5);
+// console.log(result);
+
+// result = Math.round(-5.5);
+// console.log(result);
+
+// 12. Math trunc() :
+// let result;
+
+// result = Math.trunc(87.654);
+// console.log(result);
+
+// result = Math.trunc(-87.654);
+// console.log(result);
+
+// result = Math.trunc('87.654');
+// console.log(result);
+
+// result = Math.trunc(0);
+// console.log(result);
+
+// result = Math.trunc(null);
+// console.log(result);
+
+// result = Math.trunc('Apple');
+// console.log(result);
+
+// 13. `Math max() :
+// let result;
+
+// result = Math.max(1, 5, -8, 65, 0);
+// console.log(result);
+
+// const array = [1, 2, 3, 4];
+// result = Math.max(...array);
+// console.log(result);
+
+// result = Math.max(1, 5, -8, 'apple', 65, 0);
+// console.log(result);
+
+// result = Math.max(1, 5, -8, 65, 0, '76');
+// console.log(result);
+
+// result = Math.max(1, 5, -8, Infinity, 65, 0, '76');
+// console.log(result);
+
+// 14. Math min() :
+// let result;
+
+// result = Math.min(1, 5, -8, 65, 0);
+// console.log(result);
+
+// const array = [1, 2, 3, 4];
+// result = Math.min(...array);
+// console.log(result);
+
+// result = Math.min(1, 5, -8, 'apple', 65, 0);
+// console.log(result);
+
+// result = Math.min(1, 5, -8, 65, 0, '76');
+// console.log(result);
+
+// result = Math.min(1, 5, -8, 65, -Infinity, 0, '76');
+// console.log(result);
+
+// 15. Math.sqrt() :
+// let result;
+
+// result = Math.sqrt(4);
+// console.log(result);
+
+// result = Math.sqrt(10);
+// console.log(result);
+
+// result = Math.sqrt(Infinity);
+// console.log(result);
+
+// result = Math.sqrt(-10);
+// console.log(result);
+
+// result = Math.sqrt(-Infinity);
+// console.log(result);
+
+// 16. Math sign() :
+// let result;
+
+// result = Math.sign(76);
+// console.log(result);
+
+// result = Math.sign(-76);
+// console.log(result);
+
+// result = Math.sign(0);
+// console.log(result);
+
+// result = Math.sign(-0);
+// console.log(result);
+
+// result = Math.sign(Infinity);
+// console.log(result);
+
+// result = Math.sign(-Infinity);
+// console.log(result);
+
+// result = Math.sign('76');
+// console.log(result);
+
+// result = Math.sign('-76');
+// console.log(result);
+
+// result = Math.sign('apple');
+// console.log(result);
+
+// 17. Math.log() :
+// let result;
+
+// result = Math.log(1);
+// console.log(result);
+
+// result = Math.log(0);
+// console.log(result);
+
+// result = Math.log(25);
+// console.log(result);
+
+// result = Math.log(-12);
+// console.log(result);
+
+// result = Math.log(Math.E);
+// console.log(result);
+
+// 18. Math.log2() :
+// let result;
+
+// result = Math.log2(1);
+// console.log(result);
+
+// result = Math.log2(2);
+// console.log(result);
+
+// result = Math.log2(0);
+// console.log(result);
+
+// result = Math.log2(25);
+// console.log(result);
+
+// result = Math.log2(-12);
+// console.log(result);
+
+// result = Math.log2(Math.E);
+// console.log(result);
+
+// 19. Math.log10() :
+// let result;
+
+// result = Math.log10(1);
+// console.log(result);
+
+// result = Math.log10(2);
+// console.log(result);
+
+// result = Math.log10(0);
+// console.log(result);
+
+// result = Math.log10(25);
+// console.log(result);
+
+// result = Math.log10(-12);
+// console.log(result);
+
+// result = Math.log10(Math.E);
+// console.log(result);
+
+// 20. Math.log1p() :
+// let result;
+
+// result = Math.log1p(1);
+// console.log(result);
+
+// result = Math.log1p(2);
+// console.log(result);
+
+// result = Math.log1p(0);
+// console.log(result);
+
+// result = Math.log1p(25);
+// console.log(result);
+
+// result = Math.log1p(-12);
+// console.log(result);
+
+// result = Math.log1p(Math.E);
+// console.log(result);
+
+// 21. Math.exp() :
+// let result;
+
+// result = Math.exp();
+// console.log(result);
+
+// result = Math.exp(0);
+// console.log(result);
+
+// result = Math.exp(1);
+// console.log(result);
+
+// result = Math.exp(12.567);
+// console.log(result);
+
+// result = Math.exp(-68);
+// console.log(result);
+
+// 22. Math.expm1() :
+// let result;
+
+// result = Math.expm1();
+// console.log(result);
+
+// result = Math.expm1(0);
+// console.log(result);
+
+// result = Math.expm1(1);
+// console.log(result);
+
+// result = Math.expm1(12.567);
+// console.log(result);
+
+// result = Math.expm1(-68);
+// console.log(result);
+
+// 23. Math sinh() :
+// let result;
+
+// result = Math.sinh();
+// console.log(result);
+
+// result = Math.sinh(0);
+// console.log(result);
+
+// result = Math.sinh(1);
+// console.log(result);
+
+// result = Math.sinh(12.567);
+// console.log(result);
+
+// result = Math.sinh(-68);
+// console.log(result);
+
+// result = Math.sinh(Infinity);
+// console.log(result);
+
+// result = Math.sinh(-Infinity);
+// console.log(result);
+
+// result = (Math.exp(1) - Math.exp(-1)) / 2 === Math.sinh(1);
+// console.log(result);
+
+// 24.Math cosh() :
+// let result;
+
+// result = Math.cosh();
+// console.log(result);
+
+// result = Math.cosh(0);
+// console.log(result);
+
+// result = Math.cosh(1);
+// console.log(result);
+
+// result = Math.cosh(12.567);
+// console.log(result);
+
+// result = Math.cosh(-68);
+// console.log(result);
+
+// result = Math.cosh(Infinity);
+// console.log(result);
+
+// result = Math.cosh(-Infinity);
+// console.log(result);
+
+// result = (Math.exp(1) + Math.exp(-1)) / 2 === Math.cosh(1);
+// console.log(result);
+
+// 25. Math tanh() :
+// let result;
+
+// result = Math.tanh();
+// console.log(result);
+
+// result = Math.tanh(0);
+// console.log(result);
+
+// result = Math.tanh(1);
+// console.log(result);
+
+// result = Math.tanh(12.567);
+// console.log(result);
+
+// result = Math.tanh(-68);
+// console.log(result);
+
+// result = Math.tanh(Infinity);
+// console.log(result);
+
+// result = Math.tanh(-Infinity);
+// console.log(result);
+
+// result =
+//     (Math.exp(1) - Math.exp(-1)) / (Math.exp(1) + Math.exp(-1)) === Math.tanh(1);
+// console.log(result);
+
+// 26.Math asinh() :
+// let result;
+
+// result = Math.asinh();
+// console.log(result);
+
+// result = Math.asinh(0);
+// console.log(result);
+
+// result = Math.asinh(1);
+// console.log(result);
+
+// result = Math.asinh(12.567);
+// console.log(result);
+
+// result = Math.asinh(-68);
+// console.log(result);
+
+// result = Math.asinh(Infinity);
+// console.log(result);
+
+// result = Math.asinh(-Infinity);
+// console.log(result);
+
+// 27.Math acosh() :
+// let result;
+
+// result = Math.acosh();
+// console.log(result);
+
+// result = Math.acosh(0);
+// console.log(result);
+
+// result = Math.acosh(1);
+// console.log(result);
+
+// result = Math.acosh(12.567);
+// console.log(result);
+
+// result = Math.acosh(-68);
+// console.log(result);
+
+// result = Math.acosh(Infinity);
+// console.log(result);
+
+// result = Math.acosh(-Infinity);
+// console.log(result);
+
+// 28. Math atanh() :
+// let result;
+
+// result = Math.atanh();
+// console.log(result);
+
+// result = Math.atanh(0);
+// console.log(result);
+
+// result = Math.atanh(1);
+// console.log(result);
+
+// result = Math.atanh(12.567);
+// console.log(result);
+
+// result = Math.atanh(-68);
+// console.log(result);
+
+// result = Math.atanh(Infinity);
+// console.log(result);
+
+// result = Math.atanh(-Infinity);
+// console.log(result);
+
+// 29. Math atan2() :
+// let result;
+
+// result = Math.atan2();
+// console.log(result);
+
+// result = Math.atan2(0, 5);
+// console.log(result);
+
+// result = Math.atan2(1, 5);
+// console.log(result);
+
+// result = Math.atan2(12.567, 5);
+// console.log(result);
+
+// result = Math.atan2(-68, 5);
+// console.log(result);
+
+// result = Math.atan2(Infinity, 5);
+// console.log(result);
+
+// result = Math.atan2(-Infinity, 5);
+// console.log(result);
+
+// 30. Math.fround() :
+// let result;
+
+// result = Math.fround(5);
+// console.log(result);
+
+// result = Math.fround(5.5);
+// console.log(result);
+
+// result = Math.fround(5.05);
+// console.log(result);
+
+// result = Math.fround(2.3 + 1.6);
+// console.log(result);
+
+// result = Math.fround(0);
+// console.log(result);
+
+// result = Math.fround(-8.765);
+// console.log(result);
+
+// result = Math.fround(Infinity);
+// console.log(result);
+
+// 31.Math.hypot() :
+// let result;
+
+// result = Math.hypot(0);
+// console.log(result);
+
+// result = Math.hypot(9);
+// console.log(result);
+
+// result = Math.hypot(9, 5);
+// console.log(result);
+
+// result = Math.hypot(-9, 4);
+// console.log(result);
+
+// 32. Math cbrt() :
+// let result;
+
+// result = Math.cbrt(27);
+// console.log(result);
+
+// result = Math.cbrt(0);
+// console.log(result);
+
+// result = Math.cbrt(-87);
+// console.log(result);
+
+// result = Math.cbrt(87);
+// console.log(result);
+
+// 33. Math clz32() :
+// let result;
+
+// result = Math.clz32(0);
+// console.log(result);
+
+// result = Math.clz32(1);
+// console.log(result);
+
+// result = Math.clz32(24);
+// console.log(result);
+
+// result = Math.clz32(-87);
+// console.log(result);
+
+// result = Math.clz32(-1);
+// console.log(result);
+
+// result = Math.clz32('apple');
+// console.log(result);
+
+// 34. Math random() :
+// const result = Math.random();
+// console.log(result);
+
+// random number in range min(inclusive) and max(exclusive) :
+// function getRandom(min, max) {
+//     const result = Math.random() * (max - min) + min;
+//     return result;
+// }
+// const random = getRandom(5, 50);
+// console.log(random);
+
+// random Integer in range min(inclusive) and max(exclusive) :
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     const result = Math.floor(Math.random() * (max - min)) + min;
+//     return result;
+// }
+// const random = getRandomInt(5, 50);
+// console.log(random);
+
+// random number in range min(inclusive) and max(inclusive) :
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     const result = Math.floor(Math.random() * (max - min + 1)) + min;
+//     return result;
+// }
+// const random = getRandomInt(5, 50);
+// console.log(random);
+
+
+// *** End ***
+
